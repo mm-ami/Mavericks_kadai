da2987f7998ad20ca08d60c8e9b424d04d0253c4
mm-ami <mhju.9599@gmail.com>
2022-06-09 18:29:00 +0900

    vue-final-modal追加

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,11 +6,41 @@
     <h1>{{ $auth.loggedIn }}</h1>
     <h1>{{ $auth.user }}</h1>
     <button @click="$auth.logout()">ログアウト</button>
+    <button @click="add_memo_btn">メモを追加</button>
+    <button @click="edit_btn">編集</button>
+
+    <client-only>
+      <vue-final-modal v-model="edit_additional_screen" style="background-color: rgba(0,0,0,.3); cursor: pointer">
+        <edit-additional></edit-additional>
+      </vue-final-modal>
+    </client-only>
+
   </section>
 </template>
 
 <script>
+import EditAdditional from "../components/EditAdditionalForm.vue";
+
 export default {
   name: 'IndexPage',
+
+  components: {
+    EditAdditional
+  },
+
+  data() {
+    return {
+      edit_additional_screen: false
+    }
+  },
+
+  methods: {
+    edit_btn() {
+      this.edit_additional_screen = true
+    },
+    add_memo_btn() {
+      this.edit_additional_screen = true
+    }
+  }
 }
 </script>
